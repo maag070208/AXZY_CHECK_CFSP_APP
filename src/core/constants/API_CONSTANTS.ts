f2705b0c7b1c5331f5fcd600acca4f40228341c1
martinamaro <aamaro@axzy.dev>
2026-05-02 01:13:32 -0700

    chore: update API_CONSTANTS BASE_URL to 192.168.10.106 and update yarn.lock

--- src/core/constants/API_CONSTANTS.ts
+++ src/core/constants/API_CONSTANTS.ts
@@ -1,5 +1,5 @@
 export const API_CONSTANTS = {
-  BASE_URL: 'http://192.168.10.101:4444/api/v1',
+  BASE_URL: 'http://192.168.10.106:4444/api/v1',
   // BASE_URL: 'https://axzycheckcfspapi-production.up.railway.app/api/v1',
   ROUND_COOLDOWN_MINUTES: 0,
   TIMEOUT: 5000,
